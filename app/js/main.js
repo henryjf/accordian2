2ff759fa01c61245e27179af3198119b30f496e2
JEFF HENRY <henry_jf@hotmail.com>
2016-04-14 07:50:24 -0400

    attempting to use addClass and toggle

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,10 @@ var button = $('header');
 button.on('click', function(event){
   event.preventDefault();
 console.log(button);
+var section1 = $('#para1');
+
+$(section1.addClass('#para1'))
+
+button.toggleClass('#para1');
 
 });
